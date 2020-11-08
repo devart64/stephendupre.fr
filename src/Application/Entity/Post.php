a6c61a3c09afa785c330dbbad0cdd76740fa9d79
Stephen Dupré <stephendupre64@gmail.com>
2020-11-08 11:41:27 +0100

    add the forgotten code

---
+++ src/Application/Entity/Post.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Application\Entity;
+
+use App\Application\Repository\PostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @var int $id
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+
+    /**
+     * @var string $title
+     * @ORM\Column
+     */
+    private $title;
+
+    /**
+     * @var \DateTimeImmutable $publishedAt
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $publishedAt;
+
+    /**
+     * @var string $content
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="post")
+     */
+    private $comments;
+
+    /**
+     * Post constructor.
+     */
+    public function __construct()
+    {
+        $this->publishedAt = new \DateTimeImmutable();
+        $this->comments = new ArrayCollection();
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getPublishedAt(): \DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable $publishedAt
+     */
+    public function setPublishedAt(\DateTimeImmutable $publishedAt): void
+    {
+        $this->publishedAt = $publishedAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+
+
+}
